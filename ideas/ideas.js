114ae97dfcdf231782b84801d908290c113a68a1
Tim Wissel <twtimwissel5@gmail.com>
2017-04-26 17:06:18 +0200

    Initial commit

---
+++ ideas/ideas.js
@@ -0,0 +1,67 @@
+import {Mongo} from "meteor/mongo";
+import {SimpleSchema} from "meteor/aldeed:simple-schema";
+
+Ideas = new Mongo.Collection('ideas');
+
+Ideas.allow({
+    insert() {
+        return true;
+    },
+    update() {
+        return true;
+    },
+    remove() {
+        return true;
+    }
+});
+
+const ideasSchema = new SimpleSchema({
+    title: {
+        type: String
+    },
+    owner: {
+        type: String
+    },
+    mainFocus: {
+        type: String
+    },
+    mainProposition: {
+        type: String,
+        optional: true
+    },
+    generalDescription: {
+        type: String,
+        optional: true
+    },
+    generalProblem: {
+        type: String,
+        optional: true
+    },
+    generalValue: {
+        type: String,
+        optional: true
+    },
+    generalSolution: {
+        type: String,
+        optional: true
+    },
+    stakeholders: {
+        type: [SimpleSchema.RegEx.Id]
+    },
+    company: {
+        type: SimpleSchema.RegEx.Id,
+        optional: true
+    },
+    assumptions: {
+        type: [Object],
+        optional: true
+    },
+    "assumptions.$.stakeholder": {
+        type: String
+    },
+    "assumptions.$.type": {
+        type: Number
+    }
+});
+
+Ideas.attachSchema(ideasSchema);
